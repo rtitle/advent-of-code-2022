1c12d1900c4ce911aeb1f4cc8f0e8eca0047eab6
Robert Title <rtitle@broadinstitute.org>
2022-12-05 16:09:53 -0500

    Way simpler parser using transpose

--- day5/day5.hs
+++ day5/day5.hs
@@ -11,12 +11,9 @@ data Instruction = Instruction {
 } deriving Show
 
 parseShip :: [String] -> Ship
-parseShip s = foldr inner (S.replicate len []) cols where
+parseShip s = S.fromList . fmap (filter (/= ' ')) $ transpose cols where
     s' = reverse . (drop 1) . reverse $ s
     cols = fmap (fmap (!! 1) . (chunksOf 4)) $ s'
-    len = length . head $ cols
-    inner c r = foldr (inner2) r (zip c [0..(length c)])
-    inner2 (ch, i) r = if ch == ' ' then r else S.update i (ch : (r `S.index` i)) r
 
 parseInstruction :: String -> Instruction
 parseInstruction s = Instruction (src - 1) (dest - 1) q where
