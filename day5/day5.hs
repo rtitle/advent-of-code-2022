4d4b5be4ff73de1d70f536487d0783ac2777d254
Robert Title <rtitle@broadinstitute.org>
2022-12-05 14:49:55 -0500

    Minor cleanup

--- day5/day5.hs
+++ day5/day5.hs
@@ -19,24 +19,24 @@ parseShip s = foldr inner (S.replicate len []) cols where
     inner2 (ch, i) r = if ch == ' ' then r else S.update i (ch : (r `S.index` i)) r
 
 parseInstruction :: String -> Instruction
-parseInstruction s = Instruction src dest q where
+parseInstruction s = Instruction (src - 1) (dest - 1) q where
     split = splitOn " " s 
     src = read @Int $ split !! 3
     dest = read @Int $ split !! 5
     q = read @Int $ split !! 1
 
 applyInstruction :: Bool -> Instruction -> Ship -> Ship
-applyInstruction p2 i s = (S.update (dest i - 1) newDest) . (S.update (src i - 1) newSrc) $ s where
-    moved = take (quantity i) (s `S.index` (src i - 1))
-    newSrc = drop (quantity i) (s `S.index` (src i - 1))
-    newDest = (if p2 then moved else reverse moved) ++ (s `S.index` (dest i - 1))
+applyInstruction p2 i s = (S.update (dest i) newDest) . (S.update (src i) newSrc) $ s where
+    moved = take (quantity i) (s `S.index` (src i))
+    newSrc = drop (quantity i) (s `S.index` (src i))
+    newDest = (if p2 then moved else reverse moved) ++ (s `S.index` (dest i))
 
 main :: IO ()
 main = do
     input <- readFile "day5-input.txt"
     let [shipLines, instLines] = splitWhen null $ lines input
     let initialShip = parseShip shipLines
-    let instructions = fmap parseInstruction . filter (isPrefixOf "move") $ instLines
+    let instructions = fmap parseInstruction instLines
     let part1 = fmap head $ foldr (applyInstruction False) initialShip (reverse instructions)
     let part2 = fmap head $ foldr (applyInstruction True) initialShip (reverse instructions)
     print $ part1
