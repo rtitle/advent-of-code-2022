bcfe700c12463c5a025336eafb0fafbac1c752b8
Robert Title <rtitle@broadinstitute.org>
2022-12-06 08:43:14 -0500

    Cleanup

--- day6/day6.hs
+++ day6/day6.hs
@@ -4,12 +4,16 @@ process :: Int -> String -> [String]
 process n s = scanl fn [] s where
     fn xs a =
         if length xs < n then a : xs
-        else a : (reverse . (drop 1) . reverse) xs
+        else a : init xs
+
+firstDistinct :: Int -> String -> Maybe Int
+firstDistinct n = fmap (+n) . findIndex unique . drop n . process n where
+    unique a = nub a == a
 
 main :: IO ()
 main = do
     input <- readFile "day6-input.txt"
-    let part1 = findIndex (\x -> length x == 4 && (nub x == x)) $ process 4 input
-    let part2 = findIndex (\x -> length x == 14 && (nub x == x)) $ process 14 input
+    let part1 = firstDistinct 4 input
+    let part2 = firstDistinct 14 input
     print $ part1
     print $ part2
